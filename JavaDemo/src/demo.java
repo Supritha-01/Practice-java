f6f69ec41384b27b50d13b1812508e00fdbd1577
Supritha.h <Supritha.h@DESKTOP-FE2IBD3>
2020-05-04 13:56:39 +0530

    Demo java practice

---
+++ JavaDemo/src/demo.java
@@ -0,0 +1,10 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="Please locate where'locate' occurs!";
+System.out.println("The length of the string is : " + a.indexOf("locate"));
+	}
+
+}
